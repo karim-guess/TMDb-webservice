0120348e46dba8eb9bd32fb32393f76a0be424ab
karim <karim.guessab@hotmail.com>
2025-05-22 15:25:55 +0200

    Première version du projet

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+
+//  l'API TMDb
+define('TMDB_API_KEY', '16e83367257477dc52e2b01cb02e57e6'); 
+define('TMDB_BASE_URL', 'https://api.themoviedb.org/3');
+define('TMDB_IMAGE_BASE_URL', 'https://image.tmdb.org/t/p/w500');
+
+
+define('LOG_FILE', 'searches.log');
+
+
+define('DEBUG_MODE', true);
+
+
+date_default_timezone_set('Europe/Paris');
+?>
